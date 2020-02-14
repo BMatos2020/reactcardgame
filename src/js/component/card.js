797a22c43af6448f5443563f9472a1b4b6b1e684
Brenda Matos <brenda.matosux@gmail.com>
2020-02-14 23:23:41 +0000

    card logic

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export function Card() {
+export function Card(props) {
 	return (
-		<div className="poker d-inline-block border bg-light p-1 rounded-lg">
-			<div className="h-100 d-flex justify-content-between">
+		<div className="poker d-inline-block border bg-light p-1 rounded-sm">
+			<div className="h-50 d-flex justify-content-between">
 				<span>3</span>
 				<span>♠</span>
 			</div>
-			<div className="rotate align-self-end h-100 d-flex justify-content-between">
+			<div className="rotate align-self-end h-50 d-flex justify-content-between">
 				<span>3</span>
 				<span>♠</span>
 			</div>
